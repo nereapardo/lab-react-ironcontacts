951b9d67c01a30f90c0a8c2159dbfd0c00cbdf26
nerea pardo <nerea.pardo.cundins@gmail.com>
2021-12-01 10:31:52 +0000

    lab solved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,63 @@
 import contacts from "./contacts.json";
-const contactsArray = contacts.splice(5);
+import { useState } from "react";
 
 const App = () => {
+  const [contactsArray, setContacts] = useState(contacts.slice(0, 5));
+  const [remainingContacts, setRemainingContacts] = useState(
+    contacts.slice(5, contacts.length)
+  );
+
+  const randomContact = () => {
+    let randomNum = Math.floor(Math.random() * remainingContacts.length);
+    const extractedContact = remainingContacts[randomNum];
+    const newArray = remainingContacts.filter((contact) => {
+      return contact.id !== extractedContact.id;
+    });
+
+    setRemainingContacts(newArray);
+    setContacts([...contactsArray, extractedContact]);
+  };
+
+  const sortAz = () => {
+    const sortedArray = [...contactsArray].sort((a, b) => {
+      const nameA = a.name.toUpperCase();
+      const nameB = b.name.toUpperCase();
+      return nameA < nameB ? -1 : nameA > nameB ? 1 : 0;
+    });
+
+    setContacts(sortedArray);
+  };
+
+  const sortPop = () => {
+    const sortedArray = [...contactsArray].sort((a, b) => {
+      return a.popularity - b.popularity;
+    });
+
+    setContacts(sortedArray);
+  };
+
+  const removeContact = (id) => {
+    const extractedContact = contactsArray.filter((contact) => {
+      return contact.id == id;
+    });
+
+    const newArray = contactsArray.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setRemainingContacts([...remainingContacts, extractedContact]);
+    setContacts(newArray);
+  };
+
   return (
     <div className="App">
       <h1>IronContacts</h1>
+
+      <button onClick={() => remainingContacts.length > 0 && randomContact()}>
+        Add a random contact
+      </button>
+      <button onClick={() => sortAz()}>Sort alphabetically</button>
+      <button onClick={() => sortPop()}>Sort by popularity</button>
 
       <table>
         <tr>
@@ -16,7 +69,8 @@ const App = () => {
         </tr>
 
         {contactsArray.map((person, index) => {
-          const { pictureUrl, name, popularity, wonOscar, wonEmmy } = person;
+          const { pictureUrl, name, popularity, id, wonOscar, wonEmmy } =
+            person;
           return (
             <tr key={index + Date.now()}>
               <td>
@@ -26,7 +80,13 @@ const App = () => {
               <td>{popularity}</td>
 
               {wonOscar ? <td>🏆</td> : <td></td>}
-              {wonEmmy ? <td>🏆</td> : <td></td>}
+              {wonEmmy ? <td>✨</td> : <td></td>}
+
+              <td>
+                <button onClick={() => removeContact(id)}>
+                  Remove contact
+                </button>
+              </td>
             </tr>
           );
         })}
